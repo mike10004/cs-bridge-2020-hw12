0e8cf3f780ade792bb0194302cc18c655a0bcf40
Mike Chaberski <mchaberski@gmail.com>
2020-04-04 18:11:00 -0400

    add Money definition

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,11 +1,177 @@
 // mac937@nyu.edu
-// hw12 question 1
+// hw12 
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
+
+//DISPLAY 11.9 Program Using an Array of the Money Object
+//This is the definition for the class Money.
+//Values of this type are amounts of money in U.S. currency.
+class Money
+{
+public:
+    friend Money operator +(const Money& amount1, const Money& amount2);
+    //Returns the sum of the values of amount1 and amount2.
+
+    friend Money operator -(const Money& amount1, const Money& amount2);
+    //Returns amount 1 minus amount2.
+
+    friend Money operator -(const Money& amount);
+    //Returns the negative of the value of amount.
+
+    friend bool operator ==(const Money& amount1, const Money& amount2);
+    //Returns true if amount1 and amount2 have the same value; false otherwise.
+
+    friend bool operator < (const Money& amount1, const Money& amount2);
+    //Returns true if amount1 is less than amount2; false otherwise.
+
+    Money(long dollars, int cents);
+    //Initializes the object so its value represents an amount with
+    //the dollars and cents given by the arguments. If the amount
+    //is negative, then both dollars and cents should be negative.
+
+    explicit Money(long dollars);
+    //Initializes the object so its value represents $dollars.00.
+
+    Money( );
+    //Initializes the object so its value represents $0.00.
+
+    //double get_value( ) const;
+    //Returns the amount of money recorded in the data portion of the calling
+    //object.
+
+    friend istream& operator >>(istream& ins, Money& amount);
+    //Overloads the >> operator so it can be used to input values of type
+    //Money. Notation for inputting negative amounts is as in -$100.00.
+    //Precondition: If ins is a file input stream, then ins has already been
+    //connected to a file.
+
+
+    friend ostream& operator <<(ostream& outs, const Money& amount);
+    //Overloads the << operator so it can be used to output values of type
+    //Money. Precedes each output value of type Money with a dollar sign.
+    //Precondition: If outs is a file output stream, then outs has already been
+    //connected to a file.
+private:
+    long all_cents;
+};
 
 int main() {
     cout << "q1 executed" << endl;
     return 0;
 }
+
+
+
+
+Money operator +(const Money& amount1, const Money& amount2)
+{
+    Money temp;
+    temp.all_cents = amount1.all_cents + amount2.all_cents;
+    return temp;
+}
+
+bool operator ==(const Money& amount1, const Money& amount2)
+{
+    return (amount1.all_cents == amount2.all_cents);
+}
+
+Money operator -(const Money& amount1, const Money& amount2)
+{
+    Money temp;
+    temp.all_cents = amount1.all_cents - amount2.all_cents;
+    return temp;
+}
+
+Money operator -(const Money& amount)
+{
+    Money temp;
+    temp.all_cents = -amount.all_cents;
+    return temp;
+}
+
+
+int digit_to_int(char c)
+{
+    return ( static_cast<int>(c) - static_cast<int>('0') );
+}
+
+istream& operator >>(istream& ins, Money& amount)
+{
+    char one_char, decimal_point,
+            digit1, digit2; //digits for the amount of cents
+    long dollars;
+    int cents;
+    bool negative;//set to true if input is negative.
+
+    ins >> one_char;
+    if (one_char == '-')
+    {
+        negative = true;
+        ins >> one_char; //read '$'
+    }
+    else
+        negative = false;
+    //if input is legal, then one_char == '$'
+
+    ins >> dollars >> decimal_point >> digit1 >> digit2;
+
+    if ( one_char != '$' || decimal_point != '.'
+         || !isdigit(digit1) || !isdigit(digit2) )
+    {
+        cout << "Error illegal form for money input\n";
+        exit(1);
+    }
+
+    cents = digit_to_int(digit1)*10 + digit_to_int(digit2);
+
+    amount.all_cents = dollars*100 + cents;
+    if (negative)
+        amount.all_cents = -amount.all_cents;
+
+
+    return ins;
+}
+
+
+//Uses cstdlib and iostream:
+ostream& operator <<(ostream& outs, const Money& amount)
+{
+    long positive_cents, dollars, cents;
+    positive_cents = labs(amount.all_cents);
+    dollars = positive_cents/100;
+    cents = positive_cents%100;
+
+    if (amount.all_cents < 0)
+        outs << "-$" << dollars << '.';
+    else
+        outs << "$" << dollars << '.';
+
+    if (cents < 10)
+        outs << '0';
+    outs << cents;
+
+    return outs;
+}
+
+Money::Money(long dollars, int cents) 
+        : all_cents(dollars * 100 + cents) {
+    assert((dollars >= 0 && cents >= 0) || (dollars <= 0 && cents <= 0));
+}
+
+Money::Money(long dollars) 
+        : Money(dollars, 0) 
+{
+}
+
+Money::Money() 
+        : Money(0) 
+{
+}
+
+bool operator<(const Money &amount1, const Money &amount2) {
+    return amount1.all_cents < amount2.all_cents;
+}
+
