4e17f72552dd68fc39d6ab780c9a2aa935d92fc0
Mike Chaberski <mchaberski@gmail.com>
2020-04-04 22:21:39 -0400

    possibly good implementation

--- q1/main.cpp
+++ q1/main.cpp
@@ -1,5 +1,3 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "hicpp-use-auto"
 // mac937@nyu.edu
 // hw12 
 
@@ -66,58 +64,139 @@ class Check
 public:
     Check(int number, const Money& amount, bool cashed);
     Check();
-    void set_number(int number);
-    void set_amount(const Money& amount);
-    void set_cashed(bool cashed);
+    void set_number(int number_);
+    void set_amount(const Money& amount_);
+    void set_cashed(bool cashed_);
     int get_number() const;
     Money get_amount() const;
     bool is_cashed() const;
     void print(ostream& out) const;
     void read(istream& in);
     friend ostream& operator<<(ostream& out, const Check& check);
-    friend istream& operator>>(Check& check, istream& in);
+    friend istream& operator>>(istream& in, Check& check);
 private:
     int number;
     Money amount;
     bool cashed;
 };
 
-void readChecks(Check checks[], int n) {
-    
+Check::Check(int number, const Money &amount, bool cashed) 
+        : number(number), amount(amount), cashed(cashed)
+{
+
 }
 
-void readDeposits(Money deposits[], int n) {
-    
+Check::Check() 
+        : Check(0, Money(0), false)
+{
+
 }
 
-void sortChecks(vector<Check>& checks) {
-    
+void Check::set_number(int number_) {
+    number = number_;
 }
 
-void populateCheckVector(Check source[], int numSourceChecks, vector<Check>& checks, bool cached) {
-    
+void Check::set_amount(const Money &amount_) {
+    amount = amount_;
+}
+
+void Check::set_cashed(bool cashed_) {
+    cashed = cashed_;
+}
+
+int Check::get_number() const {
+    return number;
+}
+
+Money Check::get_amount() const {
+    return amount;
+}
+
+bool Check::is_cashed() const {
+    return cashed;
+}
+
+void Check::print(ostream &out) const {
+    out << number << ' ' << amount << ' ';
+    if (cashed) {
+        out << "cashed";
+    } else {
+        out << "pending";
+    }
+}
+
+void Check::read(istream &in) {
+    int cashedInt;
+    in >> number >> amount >> cashedInt;
+    cashed = cashedInt != 0;
+}
+
+ostream &operator<<(ostream &out, const Check &check) {
+    check.print(out);
+    return out;
+}
+
+istream &operator>>(istream &in, Check &check) {
+    check.read(in);
+    return in;
+}
+
+void readChecks(Check checks[], int n, ostream& prompt, istream& in) {
+    for (int i = 0; i < n; i++) {
+        prompt << "Enter check number, amount, and cashedness (0 or 1): ";
+        in >> checks[i];
+    }
+}
+
+void readDeposits(Money deposits[], int n, ostream& prompt, istream& in) {
+    for (int i = 0; i < n; i++) {
+        prompt << "Enter deposit " << (i + 1) << " amount: ";
+        in >> deposits[i];
+    }
+}
+
+void populateCheckVector(Check source[], int numSourceChecks, vector<int>& check_positions, bool cashed) {
+    for (int i = 0; i < numSourceChecks; i++) {
+        if (source[i].is_cashed() == cashed) {
+            size_t j;
+            for (j = 0; j < check_positions.size(); j++) {
+                if (source[check_positions[j]].get_number() > source[i].get_number()) {
+                    break;
+                }
+            }
+            check_positions.insert(check_positions.begin() + j, i);
+        }
+    }
 }
 
 void printCashedAndPendingChecks(Check checks[], int n, ostream& out) {
-    vector<Check> cashed;
-    vector<Check> pending;
+    vector<int> cashed;
+    vector<int> pending;
     populateCheckVector(checks, n, cashed, true);
     populateCheckVector(checks, n, pending, false);
-    sortChecks(cashed);
     out << "Cashed checks:" << endl;
-    for (Check& check : cashed) {
-        out << check << endl;
+    for (int pos : cashed) {
+        out << checks[pos] << endl;
     }
-    sortChecks(pending);
     out << endl;
     out << "Pending checks:" << endl;
-    for (Check& check : pending) {
-        out << check << endl;
+    for (int pos : pending) {
+        out << checks[pos] << endl;
     }
+    out << endl;
 }
 
-Money calcBalance(const Money& oldBalance, Check checks[], int numChecks, Money deposits[], int numDeposits) {
-    
+Money calcNetChange(const Check checks[], int numChecks, const Money deposits[], int numDeposits) {
+    Money sum(0);
+    for (int i = 0; i < numDeposits; i++) {
+        sum = sum + deposits[i];
+    }
+    for (int i = 0; i < numChecks; i++) {
+        if (checks[i].is_cashed()) {
+            sum = sum - checks[i].get_amount();
+        }
+    }
+    return sum;
 }
 
 int main() {
@@ -126,19 +205,21 @@ int main() {
     Money newBalance;
     cout << "How many checks written? ";
     cin >> numChecks;
-    Check* checks = new Check[numChecks];
-    readChecks(checks, numChecks);
+    Check* checks = new Check[numChecks]; 
+    readChecks(checks, numChecks, cout, cin);
     cout << "How many deposits? ";
     cin >> numDeposits;
     Money* deposits = new Money[numDeposits];
-    readDeposits(deposits, numDeposits);
+    readDeposits(deposits, numDeposits, cout, cin);
     printCashedAndPendingChecks(checks, numChecks, cout);
     cout << "Enter old balance: ";
     cin >> oldBalance;
     cout << "Enter new balance: ";
     cin >> newBalance;
-    Money expectedBalance = calcBalance(oldBalance, checks, numChecks, deposits, numDeposits);
+    Money expectedBalance = oldBalance + calcNetChange(checks, numChecks, deposits, numDeposits);
     cout << "Unreconciled funds: " << (expectedBalance - newBalance) << endl;
+    delete[] checks;
+    delete[] deposits;
     return 0;
 }
 
@@ -194,12 +275,8 @@ istream& operator >>(istream& ins, Money& amount)
 
     ins >> dollars >> decimal_point >> digit1 >> digit2;
 
-    if ( one_char != '$' || decimal_point != '.'
-         || !isdigit(digit1) || !isdigit(digit2) )
-    {
-        cout << "Error illegal form for money input\n";
-        exit(1);
-    }
+    assert(!( one_char != '$' || decimal_point != '.'
+         || !isdigit(digit1) || !isdigit(digit2) ));
 
     cents = digit_to_int(digit1)*10 + digit_to_int(digit2);
 
@@ -251,5 +328,3 @@ bool operator<(const Money &amount1, const Money &amount2) {
     return amount1.all_cents < amount2.all_cents;
 }
 
-
-#pragma clang diagnostic pop
